9c52d274c09047ff75fc0a6a8e5c01d36487fe20
Kenneth Phang <bunnyppl@gmail.com>
2015-10-30 00:14:06 +0800

    updated test case

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var should = require('chai').should();
 
-var FitbitAPIoauth2 = require('fitbit-oauth2'),
+var FitbitAPIoauth2 = require('../index.js'),
         client = new FitbitAPIoauth2("dfc03f5cd3637a05939d2e47871b6532", "fefbcec6318feab8ad9cdb395b447e8b");
 
 describe('test init', function() {
